b5228cdd590baf60008d43d3dbafb7dc86fc16ed
Sanjay Kumawat <sk9785699310@gmail.com>
2025-10-07 10:55:41 +0530

    message

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./", // <-- ye line add karo
+  base: "./",
   plugins: [
     react(),
     tailwindcss()
   ],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // 👈 ye important hai
+  },
 })
